aef40c2b9ecb1f68e0d1790fae3bd606130231c9
Carson <carsonjamescook@gmail.com>
2019-12-04 22:27:02 -0800

    Updated modules to obey mypy suggestions. Also found and fixed a unit test error.

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -8,12 +8,13 @@ from traintracker.util.defs import *
 class TestServer(TestCase):
     def test_add_plot(self):
         name = "tvl"
+        id_ = 12345678
         s = Server()
-        s._add_plot(PlotType.train_val_loss, name)
-        p = s._plots[name]
+        s._add_plot(PlotType.train_val_loss, name, id_)
+        p = s._plots[id_]
         src = SOURCE_FORMATS[PlotType.train_val_loss]
         self.assertTrue(p.source.data.keys() == src.keys(),
                         "Column source for new plot should have same keys as template.")
         self.assertTrue(all([not ls for ls in p.source.data.values()]),
                         "All values (lists) for column source for new plot should be empty.")
-        self.assertFalse((not s._queues[name]), "Queue for new plot's data should have been initialized")
+        self.assertFalse((not s._queues[id_]), "Queue for new plot's data should have been initialized")
